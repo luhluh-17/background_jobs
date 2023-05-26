aad096e87d5ee83b9cf332edcea79b4589357e99
luhluh-17 <castro.ralph17@gmail.com>
2023-05-26 14:41:16 +0800

    add rescue, retry, discord on sample job

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -1,6 +1,18 @@
 class TestJob < ApplicationJob
   queue_as :default
 
+  rescue_from(ActiveRecord::RecordNotFound) do
+    puts 'Record not found'
+  end
+
+  retry_on(ActiveRecord::RecordNotFound, wait: 1.second, attempts: 3) do
+    # your handling logic in here
+  end
+
+  discard_on(ActiveRecord::RecordNotFound) do
+    # your handling logic in here
+  end
+
   def perform(user)
     "Hello #{user.name}!"
   end
